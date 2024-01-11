369fff6792829380da919e13419b16f8730fa797
guy_orlinsky <guy.orlinsky@mail.huji.ac.il>
2024-01-11 21:55:31 +0200

    button commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,31 @@
+'use client'
 import WebsiteScreen from '@/lib/components/website-screen';
 import { COURSE_WELCOME_TEXT } from '@/lib/config';
 import Image from 'next/image';
 import Link from 'next/link';
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [isSectionVisible, setSectionVisibility] = useState(false);
+
+  const turnOnSection = () => {
+    setSectionVisibility(true);
+  };
+
+  const turnOffSection = () => {
+    setSectionVisibility(false);
+  };
+
   return (
       
       <div className="grid"  style={{ backgroundColor: 'yellow' }}>
+
         <div className="center padding-2 text-xl">
           <span>
             {COURSE_WELCOME_TEXT}
           </span>
         </div>
+
         <div className="center padding-2">
           <Image
             src="/us_flag_and_statue_of_liberty.jpg"
@@ -21,6 +35,7 @@ export default function Home() {
             priority
           />
         </div>
+
         <div className="center padding-2">
           <Image
             src="/brown_bear.jpeg"
@@ -31,11 +46,12 @@ export default function Home() {
             priority
           />
         </div>
+
         <div>
           <h1 className="center text-xxxl">
             
             when I grow up I want to . . .
-    
+
           </h1>
       
           <ul>
@@ -48,15 +64,27 @@ export default function Home() {
           <div className="center padding-2">
           <p>here is the wikipedia page of my role model</p>
           </div>
+
+          <div className="center padding-2">
+                <button onClick={turnOnSection}> click to go to the moon! </button>
+          </div>
           
           <div className="center padding-2">
-              <button id="moonButton">Press to go to the moon</button>
               <iframe src="https://en.wikipedia.org/wiki/Donald_Trump"  height="200" width="900" title='here is my role model'></iframe>
           </div>
-          <div className="center padding-2">
-          </div>
+
+          {isSectionVisible && (
+              <div className="center padding-2">
+              <p>moons in our solar system</p>
+              <br></br>
+              <iframe src="https://science.nasa.gov/gallery/moons/"  height="500" width="900" title='moons of our solar system'></iframe>
+              <br></br>
+            <button onClick={turnOffSection}>close</button>
+            </div>
+            )}
 
         </div>
+
       </div>
   )
 }
